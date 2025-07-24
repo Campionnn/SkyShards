948ab9e7f16bed3dde173b09811f77781e43d55b
Mischa <mischa.doerfler@gmail.com>
2025-07-24 19:27:12 +0200

    use capital letters in formatLargeNubmer

--- src/utilities/utilityFunctions.ts
+++ src/utilities/utilityFunctions.ts
@@ -44,13 +44,13 @@ export const getRarityColor = (rarity: string): string => {
 
 export const formatLargeNumber = (num: number): string => {
   if (num >= 1000000000) {
-    return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "b";
+    return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "B";
   }
   if (num >= 1000000) {
-    return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "m";
+    return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
   }
   if (num >= 1000) {
-    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "k";
+    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
   }
   return num.toFixed(2);
 };
