861d9f9b7c9b5efb231b91c5b74c1603515530a8
Campion <ryanchien2468@gmail.com>
2026-01-21 23:11:01 -0800

    update privacy policy

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -41,13 +41,6 @@ const PrivacyPolicy: React.FC = () => (
     </ul>
 
     <Divider />
-    <h2 className="text-xl font-semibold mb-2 text-slate-300">Cookies</h2>
-    <ul className="list-disc pl-6 mb-4 text-slate-400">
-      <li>Cookies may be used for basic functionality (e.g., remembering your settings).</li>
-      <li>No tracking cookies for advertising or profiling.</li>
-    </ul>
-
-    <Divider />
     <h2 className="text-xl font-semibold mb-2 text-slate-300">Third-Party Services</h2>
     <ul className="list-disc pl-6 mb-4 text-slate-400">
       <li>Links to third-party sites (e.g., Ko-fi) are for your convenience.</li>
@@ -63,6 +56,61 @@ const PrivacyPolicy: React.FC = () => (
     <Divider />
     <h2 className="text-xl font-semibold mb-2 text-slate-300">Policy Updates</h2>
     <p className="mb-4 text-slate-400">This policy may be updated as needed. Please check back for changes.</p>
+
+    <Divider />
+    <h2 className="text-xl font-semibold mb-2 text-slate-300">Mediavine Programmatic Advertising (Ver 1.1)</h2>
+    <p className="mb-4 text-slate-400">
+      The Website works with Mediavine to manage third-party interest-based advertising appearing on the Website. Mediavine serves content and advertisements when you visit the Website, which may use first and third-party cookies. A cookie is a small text file which is sent to your computer or mobile device (referred to in this policy as a “device”) by the web server so that a website can remember some information about your browsing activity on the Website.
+    </p>
+    <p className="mb-4 text-slate-400">
+      First party cookies are created by the website that you are visiting. A third-party cookie is frequently used in behavioral advertising and analytics and is created by a domain other than the website you are visiting. Third-party cookies, tags, pixels, beacons and other similar technologies (collectively, “Tags”) may be placed on the Website to monitor interaction with advertising content and to target and optimize advertising. Each internet browser has functionality so that you can block both first and third-party cookies and clear your browser’s cache. The "help" feature of the menu bar on most browsers will tell you how to stop accepting new cookies, how to receive notification of new cookies, how to disable existing cookies and how to clear your browser’s cache. For more information about cookies and how to disable them, you can consult the information at{" "}
+      <a href="https://www.allaboutcookies.org/manage-cookies/" target="_blank" rel="noreferrer noopener nofollow" className="text-amber-300/70 underline">
+        All About Cookies
+      </a>
+      .
+    </p>
+    <p className="mb-4 text-slate-400">
+      Without cookies you may not be able to take full advantage of the Website content and features. Please note that rejecting cookies does not mean that you will no longer see ads when you visit our Site. In the event you opt-out, you will still see non-personalized advertisements on the Website.
+    </p>
+    <p className="mb-4 text-slate-400">The Website collects the following data using a cookie when serving personalized ads:</p>
+    <ul className="list-disc pl-6 mb-4 text-slate-400">
+      <li>IP Address</li>
+      <li>Operating System type</li>
+      <li>Operating System version</li>
+      <li>Device Type</li>
+      <li>Language of the website</li>
+      <li>Web browser type</li>
+      <li>Email (in hashed form)</li>
+    </ul>
+    <p className="mb-4 text-slate-400">
+      Mediavine Partners (companies listed below with whom Mediavine shares data) may also use this data to link to other end user information the partner has independently collected to deliver targeted advertisements. Mediavine Partners may also separately collect data about end users from other sources, such as advertising IDs or pixels, and link that data to data collected from Mediavine publishers in order to provide interest-based advertising across your online experience, including devices, browsers and apps. This data includes usage data, cookie information, device information, information about interactions between users and advertisements and websites, geolocation data, traffic data, and information about a visitor’s referral source to a particular website. Mediavine Partners may also create unique IDs to create audience segments, which are used to provide targeted advertising.
+    </p>
+    <p className="mb-4 text-slate-400">
+      If you would like more information about this practice and to know your choices to opt-in or opt-out of this data collection, please visit{" "}
+      <a href="https://thenai.org/opt-out/" target="_blank" rel="noreferrer noopener nofollow" className="text-amber-300/70 underline">
+        National Advertising Initiative opt out page
+      </a>
+      . You may also visit{" "}
+      <a href="http://optout.aboutads.info/#/" target="_blank" rel="noreferrer noopener nofollow" className="text-amber-300/70 underline">
+        Digital Advertising Alliance website
+      </a>{" "}
+      and{" "}
+      <a href="http://optout.networkadvertising.org/#" target="_blank" rel="noreferrer noopener nofollow" className="text-amber-300/70 underline">
+        Network Advertising Initiative website
+      </a>{" "}
+      to learn more information about interest-based advertising. You may download the AppChoices app at{" "}
+      <a href="https://youradchoices.com/appchoices" target="_blank" rel="noreferrer noopener nofollow" className="text-amber-300/70 underline">
+        Digital Advertising Alliance’s AppChoices app
+      </a>{" "}
+      to opt out in connection with mobile apps, or use the platform controls on your mobile device to opt out.
+    </p>
+    <p className="mb-4 text-slate-400">
+      For specific information about Mediavine Partners, the data each collects and their data collection and privacy policies, please visit{" "}
+      <a href="https://www.mediavine.com/ad-partners/" target="_blank" rel="noreferrer noopener nofollow" className="text-amber-300/70 underline">
+        Mediavine Partners
+      </a>
+      .
+    </p>
   </div>
 );
 
